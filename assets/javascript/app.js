b046ced1827fe2f71309b640280eb5efee1652e6
unknown <danie@Dan-Laptop>
2018-06-26 14:42:20 -0500

    testing links

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+var buttonTimer = document.getElementsByClassName("col-md-12-buttonTimer");
+
+console.log(buttonTimer);
+
+buttonTimer.onclick = function() {
+    buttonTimer.innerHTML = "<h2>Timer goes here</h2>";
+}
