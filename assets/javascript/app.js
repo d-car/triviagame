4269059d100b80615cf68f814747bfcd43e75f4e
d-car <danielscarstensen@gmail.com>
2018-06-28 00:46:31 -0500

    display functional. working on readio buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,83 @@
 var correct = 0;
 var incorrect = 0;
-var timerNumber = 60;
+var timerNumber = 10;
 var buttonTimer = document.getElementById("buttonTimer");
 var startButton = document.getElementById("startGame");
+var gameArea = document.getElementById("gameArea");
 var a1 = document.getElementById("q1-2");
 var a2 = document.getElementById("q2");
 var a3 = document.getElementById("q3");
 var a4 = document.getElementById("q4");
 var a5 = document.getElementById("q5");
 var a6 = document.getElementById("q6");
-var gameStart = false;
+var gameStatus = false;
+
+//=================================================================================================================
 
 
-$("#startButton").click(runGame())   
+// // Hides questions before game starts
+// function hideBoard() {
+//     if (gameStatus = false) {
+//         // $("startGame").hide();
+//         $("#gameArea").text("Get Started!");
+//     }
+// }
 
+// Resets round score and timer
+function reset() {
+    gameStatus = false;
+    correct = 0;
+    incorrect = 0;
+    timerNumber = 10;
+
+}  
+
+// Actually uns the clock down
 function runClock() {
     clearInterval(timerNumber);
-    intervalId = setInterval(decrement, 1000);
+    intervalId = setInterval(countDown, 1000);
   }
 
-function decrement() {
-    timerNumber--;
-    $("#timer").html("<h2>" + timerNumber + "</h2>");
-    if (timerNumber === 0) {
-        stopGame();
-        alert("Time Up! Let's See How Ya Did");
-        results();
-    }
+// Sets up countdown instructions and calls stopGame();, alerts that time is up, and calls results();
+function countDown() {
+        $("#startGame").hide();
+        timerNumber--;
+        $("#timer").html("<h2>" + timerNumber + " Seconds Left!" + "</h2>");
+        if (timerNumber === 0) {
+            stopGame();
+            alert("Time's Up! Let's See How Ya Did");
+            results();
+        }
 }
 
+// Clears Timer
 function stopGame() {
     clearInterval(intervalId);
 }
 
-function ready() {
-    document.gameStart;
-}
-
+// Runs Game.  This is actually called in the html with the onClick att.
 function runGame() {
-    var gameStart = true;
-    if (gameStart = true) {
-        displayAll()
-    }
-    else {
-        $(".lead").text("False"); 
-    }
+    gameStatus = true;
+    countDown();
+    runClock();
+    displayBoard();
+    console.log(gameStatus);
 }
 
-function displayAll() {
+
+// DISPLAYS THE QUESTIONS AND ANSWERS
+function displayBoard() {
     $(".lead").text("Instructions go here.");
+    $("#gameArea").show(gameArea);
 }
 
-function startTimer() {
 
-}
+// Hides #gameArea until runGame(); is called
+$(document).ready(function() {
+    $("#gameArea").hide();
+})
 
-runClock()
-console.log(timerNumber)
-
-
+console.log(gameStatus);
 
 
 
